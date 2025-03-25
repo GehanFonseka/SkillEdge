4ef9d2bf7c44355b241189b703f99fb66b4daa38
kavishka <kavishkadeshan@428gmail.com>
2025-03-25 11:17:25 +0530

    Update App.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -4,6 +4,7 @@ import { Routes, Route, useNavigate } from "react-router-dom";
 import Header from "./components/home/Header";
 import Footer from "./components/home/Footer";
 import Home from "./components/home/Home";
+
 import Login from "./components/login/Login";
 import Register from "./components/login/Register";
 import api from "./api/axiosConfig";
