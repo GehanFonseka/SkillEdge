3b0523496c5b67a993f9e45e8e638d08d9061f30
GehanFonseka <gehanfonseka@gmail.com>
2025-04-27 03:08:37 +0530

    Revert "update achievments"
    This reverts commit 007dd29f933ba75ac5b32f417ea19ca329905e87.

--- Frontend/src/Pages/AchievementsManagement/AddAchievements.js
+++ Frontend/src/Pages/AchievementsManagement/AddAchievements.js
@@ -10,10 +10,9 @@ function AddAchievements() {
     postOwnerID: '',
     category: '',
     postOwnerName: '',
-  });                                     
+  });
   const [image, setImage] = useState(null);
   const [imagePreview, setImagePreview] = useState(null);
-  
 
   const handleImageChange = (e) => {
     const file = e.target.files[0];
--- Frontend/src/index.js
+++ Frontend/src/index.js
@@ -1,19 +1,16 @@
-import React from "react"; // Import React
-import ReactDOM from "react-dom/client"; // Import ReactDOM
-import { BrowserRouter } from "react-router-dom"; // Import BrowserRouter
-import App from "./App"; // Import App component
-import "./index.css"; // Import global styles
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { BrowserRouter } from "react-router-dom";
+import { createRoot } from "react-dom/client";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+const root = createRoot(document.getElementById("root"));
+
 root.render(
-  <React.StrictMode>
-    <BrowserRouter
-      future={{
-        v7_startTransition: true,
-        v7_relativeSplatPath: true,
-      }}
-    >
+  <BrowserRouter>
+    <React.StrictMode>
       <App />
-    </BrowserRouter>
-  </React.StrictMode>
+    </React.StrictMode>
+  </BrowserRouter>
 );
